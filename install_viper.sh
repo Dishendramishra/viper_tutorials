db66a7dea6313437fb550d80b9b9eebded270df5
Live session user <dishendramishra@gmail.com>
2017-11-16 10:09:49 +0000

    added install_viper.sh

---
+++ install_viper.sh
@@ -0,0 +1,18 @@
+# getting root access
+su
+
+#Installing dependencies:
+sudo apt-get install build-essential libssl-dev libffi-dev libgmp-dev libsecp256k1-dev
+sudo apt-get install python3-pip git 
+
+#Installing python dependencies:
+pip3 install coincurve bitcoin
+
+#cloning viper repo
+git clone https://github.com/ethereum/viper.git
+cd viper
+
+# installing viper
+sudo python3 setup.py install
+
+
